97c7d202bcd3a698aa1a3097f91cad2a781ad1ab
JanPro740 <janidushanaka456@gmail.com>
2025-01-03 09:33:56 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "", // Add your session id
+SESSION_ID: process.env.SESSION_ID || "K3RmHZba#f2N4CWsY0Mtb3t4R8jN_BiOhTxoeM31t3NZ7M69AkLw", // Add your session id
 PREFIX: process.env.PREFIX || ".",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "public",
